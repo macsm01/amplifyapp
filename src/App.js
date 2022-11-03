4b80f2375c51a5d22034b0311ebe41d54eba28da
macsm01 <113207619+macsm01@users.noreply.github.com>
2022-11-03 14:16:12 +0000

    Removed checkbox for data generation en masse

--- src/App.js
+++ src/App.js
@@ -505,8 +505,7 @@ function App({ signOut, user }) {
   <br></br>
   <button className = "genBtn" id="genBtn" type="button" onClick={generate1Payment}>Generate Payment</button>
   <button className = "genBtn" id="subBtn" type="submit">Submit Payment</button>
-  <label for="chkGenPayments" class="chkGenPayments">Generate 50 random payments (for testing only) </label>
-  <input type="checkbox" id="chkGenPayments" classname="genBtn"/>
+  <br/><br/>
         
 
 </form>
